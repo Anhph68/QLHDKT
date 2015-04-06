ffa84de9eb3390c2fffa35b2051a1e0cc780b101
Alfa <Anhph68@gmail.com>
2015-04-06 17:48:21 +0700

    add Angularjs and Angular Bootstrap UI

---
+++ KTNN/Controllers/KNKTController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KTNN.Controllers
+{
+    public class KNKTController : Controller
+    {
+        // GET: KNKT
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
